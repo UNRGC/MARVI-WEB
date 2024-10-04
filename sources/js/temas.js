ac47a956ad1072e555423bff9ffc4d7a1a899725
Ulises Noriega <resvmm@gmail.com>
2024-10-03 20:28:20 -0600

    Creación del inicio y pequeñas correcciones

--- sources/js/temas.js
+++ sources/js/temas.js
@@ -77,13 +77,15 @@ function aplicarColores(colorPrimario, colorSecundario) {
 function modoOscuro(activo) {
     if (activo == "true" || activo === true) {
         document.documentElement.style.setProperty("--fondo", "var(--fondo-oscuro)");
+        document.documentElement.style.setProperty("--fondo-contenido", "var(--fondo-contenido-oscuro)");
         document.documentElement.style.setProperty("--borde", "var(--borde-oscuro)");
         document.documentElement.style.setProperty("--hover-borde", "var(--hover-borde-oscuro)");
         document.documentElement.style.setProperty("--texto", "var(--texto-oscuro)");
         document.documentElement.style.setProperty("--placeholder", "var(--placeholder-oscuro)");
-        document.documentElement.style.setProperty("--fondo-recovery", "var(--fondo-oscuro)");
+        document.documentElement.style.setProperty("--fondo-recovery", "var(--fondo-contenido-oscuro)");
     } else {
         document.documentElement.style.setProperty("--fondo", "var(--fondo-claro)");
+        document.documentElement.style.setProperty("--fondo-contenido", "var(--fondo-contenido-claro)");
         document.documentElement.style.setProperty("--borde", "var(--borde-claro)");
         document.documentElement.style.setProperty("--hover-borde", "var(--hover-borde-claro)");
         document.documentElement.style.setProperty("--texto", "var(--texto-claro)");
@@ -91,6 +93,14 @@ function modoOscuro(activo) {
         document.documentElement.style.setProperty("--fondo-recovery", "var(--fondo-img)");
     }
     localStorage.setItem("modoOscuro", activo);
+}
+
+// Función para convertir un color hexadecimal a rgba
+function hexToRgba(hex, alpha) {
+    let r = parseInt(hex.slice(1, 3), 16);
+    let g = parseInt(hex.slice(3, 5), 16);
+    let b = parseInt(hex.slice(5, 7), 16);
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
 }
 
 // Agrega un listener para recibir el mensaje y recargar la página
