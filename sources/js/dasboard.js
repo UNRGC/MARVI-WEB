fcf2906e9da1da8a09b70fb02f816b45696b3bec
Ulises Noriega <resvmm@gmail.com>
2024-10-10 22:51:46 -0600

    Creación de las alertas de confirmación

--- sources/js/dasboard.js
+++ sources/js/dasboard.js
@@ -23,24 +23,12 @@ function mostrarPedidos() {
     offcanvas.show();
 }
 
-
 function mostrarRegistrarCliente() {
     var offcanvasElement = document.getElementById("offcanvasRegistrarCliente");
     var offcanvas = new bootstrap.Offcanvas(offcanvasElement);
     offcanvas.show();
 }
 
-function mostrarEliminarPedido() {
-    var offcanvasElement = document.getElementById("offcanvasEliminarPedido");
-    var offcanvas = new bootstrap.Offcanvas(offcanvasElement);
-    offcanvas.show();
-}
-
-function mostrarEliminarCliente() {
-    var offcanvasElement = document.getElementById("offcanvasEliminarCliente");
-    var offcanvas = new bootstrap.Offcanvas(offcanvasElement);
-    offcanvas.show();
-}
 /* ------- Función para escuchar el mensaje ------- */
 window.addEventListener("message", function (event) {
     if (event.data === "mostrarClientes") {
@@ -53,10 +41,79 @@ window.addEventListener("message", function (event) {
         mostrarRegistrarCliente();
     }
     if (event.data === "mostrarEliminarPedido") {
-        mostrarEliminarPedido();
+        eliminarRegistro();
     }
     if (event.data === "mostrarEliminarCliente") {
-        mostrarEliminarCliente();
+        eliminarRegistro();
+    }
+    if (event.data === "salir") {
+        salir();
     }
     /* En caso de haber más offcanvas condicionar aquí*/
 });
+
+/* ------- Funciones para crear alertas ------- */
+function eliminarRegistro() {
+    Swal.fire({
+        title: "¿Estás seguro?",
+        text: "No podrás revertir esto.",
+        icon: "warning",
+        iconColor: "#dd5746",
+        showCancelButton: true,
+        confirmButtonText: "Sí, eliminar",
+        cancelButtonText: "Cancelar",
+    }).then((result) => {
+        if (result.isConfirmed) {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: "bottom-end",
+                showConfirmButton: false,
+                timer: 2000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                },
+            });
+            Toast.fire({
+                icon: "success",
+                iconColor: "#4caf50",
+                title: "Registro eliminado con éxito.",
+            });
+        }
+    });
+}
+
+function salir() {
+    Swal.fire({
+        title: "¿Quieres cerrar sesión?",
+        text: "Estas a punto de salir del sistema.",
+        icon: "question",
+        iconColor: "#dd5746",
+        showCancelButton: true,
+        confirmButtonText: "Si, salir",
+        cancelButtonText: "Cancelar",
+    }).then((result) => {
+        if (result.isConfirmed) {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: "bottom-end",
+                showConfirmButton: false,
+                timer: 2000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                },
+                didClose: () => {
+                    window.location.href = "login.html";
+                },
+            });
+            Toast.fire({
+                icon: "info",
+                iconColor: "#4caf50",
+                title: "Saliendo del sistema...",
+            });
+        }
+    });
+}
